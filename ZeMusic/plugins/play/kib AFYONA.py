4e25bfa161881bdcc21297dcd25a686b6cfd69d4
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-02-24 02:34:48 +0200

    Update kib AFYONA.py

--- ZeMusic/plugins/play/kib AFYONA.py
+++ ZeMusic/plugins/play/kib AFYONA.py
@@ -1,6 +1,6 @@
 import asyncio
 from pyrogram import Client, filters
-from strings import get_command
+from strings import get_string
 from strings.filters import command
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyKeyboardMarkup
 from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
